d3594cf8dc6012debc2347de0c113935fc1bf670
husnain572 <111906845+husnain572@users.noreply.github.com>
2025-04-26 16:39:09 +0500

    Time: 0 ms (100.00%) | Memory: 8.6 MB (54.24%) - LeetSync

---
+++ 7-reverse-integer/reverse-integer.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int reverse(int x) {
+      int reverse_number=0;
+        while(x!=0){
+            int digit=x%10;
+            if(reverse_number>INT_MAX/10 ||reverse_number<INT_MIN/10){
+                return 0;
+            }
+            reverse_number=reverse_number*10+digit;
+            x=x/10;
+        }
+        return reverse_number;
+    }
+};
