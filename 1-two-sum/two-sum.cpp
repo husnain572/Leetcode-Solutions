9f0f2120d2231d8b580bf08194ed08a7db0b0285
husnain572 <111906845+husnain572@users.noreply.github.com>
2025-04-30 15:58:08 +0500

    Time: 1 ms (81.43%) | Memory: 14.9 MB (36.26%) - LeetSync

--- 1-two-sum/two-sum.cpp
+++ 1-two-sum/two-sum.cpp
@@ -1,7 +1,25 @@
 class Solution {
 public:
+  vector<int> twoSum(vector<int>& nums, int target) {
+    unordered_map<int,int> m;
+    vector<int> ans;
+    for(int i=0;i<nums.size();i++){
+        int first=nums[i];
+        int second=target-first;
+        if(m.find(second)!=m.end()){
+            ans.push_back(i);
+            ans.push_back(m[second]);
+            break;
+        }
+        m[first]=i;
+    }
+    return ans;
+  }
+  };
+
+/*
     // Two pointers - optimized approach
-    vector<int> twoSum(vector<int>& nums, int target) {
+
         int n=nums.size();
         vector<pair<int,int>> res;
         int i=0;
@@ -28,7 +46,7 @@ public:
     }
      return {}; 
     }
-};
+*/
 
 
 // Brute Force Approach
