575b7405cffcd13184287c005791c13217ba340c
husnain572 <111906845+husnain572@users.noreply.github.com>
2025-09-13 21:49:28 +0500

    Time: 51 ms (57.11%) | Memory: 90.7 MB (71.16%) - LeetSync

--- 217-contains-duplicate/contains-duplicate.cpp
+++ 217-contains-duplicate/contains-duplicate.cpp
@@ -1,11 +1,11 @@
 class Solution {
 public:
     bool containsDuplicate(vector<int>& nums) {
-       unordered_set<int> st;
-       for(auto itr:nums) {
+        unordered_set<int>st;
+        for(int itr:nums){
             if(st.count(itr)) return true;
             st.insert(itr);
-       }
-       return false;
+        }
+        return false;
     }
 };
