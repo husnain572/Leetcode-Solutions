12ead6960730ffba52371d7d89353d65de68b48c
husnain572 <111906845+husnain572@users.noreply.github.com>
2026-01-06 19:38:54 +0500

    Time: 42 ms (85.74%) | Memory: 29.2 MB (45.45%) - LeetSync

--- 15-3sum/3sum.cpp
+++ 15-3sum/3sum.cpp
@@ -1,35 +1,31 @@
 class Solution {
 public:
     vector<vector<int>> threeSum(vector<int>& nums) {
-     vector<vector<int>>res;
-     sort(nums.begin(),nums.end());
-     int n=nums.size();
-     for(int i=0;i<n;i++){
-        if(i>0 && nums[i]==nums[i-1]){
-            continue;
-        }
-        int j=i+1;
-        int k=n-1;
-        while(j<k){
-            int sum=nums[i]+nums[j]+nums[k];
-            if(sum>0){
-                k--;
-            }else if(sum<0){
-                j++;
-            }else{
-                res.push_back({nums[i],nums[j],nums[k]});
-                j++;
-                while(nums[j]==nums[j-1] && j<k){
+        sort(nums.begin(), nums.end());
+        vector<vector<int>> res;
+        for (int i = 0; i < nums.size(); i++) {
+            if (i > 0 && nums[i] == nums[i - 1]) {
+                continue;
+            }
+        int k = nums.size() - 1;
+            int j = i+1;
+
+            while (j < k) {
+                int total = nums[i] + nums[j] + nums[k];
+                if (total > 0) {
+                    k--;
+                } else if (total < 0) {
                     j++;
+                } else {
+                    res.push_back({nums[i], nums[j], nums[k]});
+                    j++;
+                    k--;
+                    while (j < k && nums[j] == nums[j - 1]) j++;
+                    while (j < k && nums[k] == nums[k + 1]) k--;
+
                 }
             }
-
         }
-     }   
-     return res;
+        return res;
     }
 };
-
-
-// TC : O(nlogn+n^2)
-// SC :(uniqueTriplets)
