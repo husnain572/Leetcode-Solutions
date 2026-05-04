a74fab9986282aa558f975ff3f21e70da1ad4ac1
husnain572 <111906845+husnain572@users.noreply.github.com>
2026-05-04 21:57:09 +0500

    Time: 0 ms (100.00%) | Memory: 8.6 MB (37.32%) - LeetSync

--- 9-palindrome-number/palindrome-number.cpp
+++ 9-palindrome-number/palindrome-number.cpp
@@ -1,17 +1,22 @@
 class Solution {
 public:
     bool isPalindrome(int x) {
+        int orig_val = x;
+        int rev_num = 0;
         if (x < 0)
             return false;
-
-        int original = x;
-        long long res = 0; 
-
-        while (x > 0) {
-            res = res * 10 + x % 10;
+        while (x != 0) {
+            int last_dig = x % 10;
+          if(rev_num>INT_MAX/10 || rev_num<INT_MIN){
+            return false;
+          }
+            rev_num = rev_num * 10 + last_dig;
             x = x / 10;
         }
-
-        return res == original;
+        if (rev_num == orig_val) {
+            return true;
+        } else {
+            return false;
+        }
     }
 };
