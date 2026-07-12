5b5425bd847e2d9b8a9298a4dbb17c2524a29160
husnain572 <111906845+husnain572@users.noreply.github.com>
2026-07-12 23:06:13 +0500

    Time: 0 ms (100.00%) | Memory: 11.5 MB (98.06%) - LeetSync

--- 75-sort-colors/sort-colors.cpp
+++ 75-sort-colors/sort-colors.cpp
@@ -6,16 +6,17 @@ public:
         int high=nums.size()-1;
         while(mid<=high){
             if(nums[mid]==0){
-                swap(nums[low],nums[mid]);
+                swap(nums[mid], nums[low]);
+                mid++;
                 low++;
+            }else if(nums[mid]==1){
                 mid++;
             }
-            else if(nums[mid]==1){
-                    mid++;
-            }else{
+            else{
                 swap(nums[mid], nums[high]);
+              
                 high--;
-            } 
+            }
         }
     }
 };
