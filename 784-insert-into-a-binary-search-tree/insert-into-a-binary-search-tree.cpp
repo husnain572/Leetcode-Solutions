d17c1df6172fd318dbc5be621fa9b6901fe717bb
husnain572 <111906845+husnain572@users.noreply.github.com>
2026-09-21 20:41:08 +0500

    Time: 0 ms (100.00%) | Memory: 59.5 MB (24.83%) - LeetSync

---
+++ 784-insert-into-a-binary-search-tree/insert-into-a-binary-search-tree.cpp
@@ -0,0 +1,45 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode* insertIntoBST(TreeNode* root, int val) {
+
+        // Empty position found
+        if (root == NULL) {
+            return new TreeNode(val);
+        }
+
+        // Go to left subtree
+        if (val < root->val) {
+            root->left = insertIntoBST(root->left, val);
+        }
+
+        // Go to right subtree
+        else {
+            root->right = insertIntoBST(root->right, val);
+        }
+
+        // Return the root of this subtree
+        return root;
+    }
+};
